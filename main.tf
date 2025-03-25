a2cc4ace5223b597d5891fb0f2bf6b752ef77e2f
RohiniAlla <rohini.it@gmail.com>
2025-03-25 13:20:59 -0500

    added files

---
+++ main.tf
@@ -0,0 +1,26 @@
+provider "aws" {
+  region = "us-west-1"
+  access_key = "AWS_ACCESS_KEY_ID"
+  secret_key = "AWS_SECRET_ACCESS_KEY"
+}
+
+resource "aws_instance" "my-instance" {
+  ami = "ami-0a15a308c19bc4970"
+  instance_type = "t2.micro"
+
+  tags = {
+   Name = "ec2 instance1"
+  }
+} 
+
+resource "aws_s3_bucket" "my_s3_bucket" {
+  bucket = "mys3buckettes"
+  #acl = "private"
+  tags = {
+    Name = "mys3buckettes"
+  }
+}
+
+
+
+
